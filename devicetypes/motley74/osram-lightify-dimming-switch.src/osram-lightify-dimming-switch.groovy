6a708e1b3d0385c8c51386b212c86fceddbbf6c7
Michael Hudson <motley74@gmail.com>
2016-02-14 23:10:36 -0700

    rework parse logic for OSRAM Lightify Dimmer Switch

--- devicetypes/motley74/osram-lightify-dimming-switch.src/osram-lightify-dimming-switch.groovy
+++ devicetypes/motley74/osram-lightify-dimming-switch.src/osram-lightify-dimming-switch.groovy
@@ -18,10 +18,12 @@
 metadata {
   definition (name: "OSRAM Lightify Dimming Switch", namespace: "motley74", author: "Michael Hudson") {
     capability "Actuator"
+    capability "Battery"
     capability "Button"
     capability "Configuration"
     capability "Refresh"
     capability "Sensor"
+    capability "Temperature Measurement"
 
     fingerprint profileId: "0104", deviceId: "0001", inClusters: "0000, 0001, 0003, 0020, 0402, 0B05", outClusters: "0003, 0006, 0008, 0019" //, manufacturer: "OSRAM", model: "Lightify 2.4GHZZB/SWITCH/LFY", deviceJoinName: "OSRAM Lightify Dimming Switch"
   }
@@ -29,10 +31,18 @@ metadata {
   simulator {
     // TODO: define status and reply messages here
   }
+  
+  preferences {
+    input title: "Temperature Offset", description: "This feature allows you to correct any temperature variations by setting an offset. Ex: If your sensor consistently reports a temp that's 5 degrees too warm, you'd enter \"-5\". If 3 degrees too cold, enter \"+3\".", displayDuringSetup: false, type: "paragraph", element: "paragraph"
+    input "tempOffset", "number", title: "Degrees", description: "Adjust temperature by this many degrees", range: "*..*", displayDuringSetup: false
+  }
 
   tiles(scale: 2) {
     standardTile("button", "device.button", width: 6, height: 4) {
       state "default", label: "", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"
+    }
+    valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+      state "battery", label:'${currentValue}% battery'
     }
     valueTile("temperature", "device.temperature", width: 2, height: 2) {
       state("temperature", label:'${currentValue}°',
@@ -58,32 +68,54 @@ metadata {
     standardTile("refresh", "device.button", decoration: "flat", width: 2, height: 2) {
       state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
     }
-    main "button"
-    details(["button", "temperature", "refresh"])
+    main "temperature"
+    details(["button", "temperature", "battery", "refresh"])
   }
 }
 
 def parse(String description) {
+  Map map = [:]
   log.debug "parse description: $description"
-    
+  // Call proper method based on command received
+  if (description?.startsWith('catchall:')) {
+    map = parseCatchAllMessage(description)
+  } else if (description?.startsWith('read')) {
+    def msg = zigbee.parseDescriptionAsMap(description)
+    log.debug msg
+    map = getBatteryResult(msg.value)
+  } else if (description?.startsWith('temperature:')) {
+    map = parseCustomMessage(description)
+  } else {
+    log.debug "Unknown message received, parsed message: $msg"
+  }
+  return map
+}
+
+private Map parseCatchAllMessage(String description) {
+  Map resultMap = [:]
   // Create a map from the raw zigbee message to make parsing more intuitive
   def msg = zigbee.parse(description)
-  log.debug msg
-  
-  if (description?.startsWith('catchall:') && (msg.clusterId == 6 || msg.clusterId == 8)) {
-    // Call proper method based on command received
-    log.debug "Command is $msg.command and data is $msg.data"
-    if ((msg.command==0) || (msg.command==1 && !msg.data)) {
-      lightPower(msg.command)
-    } else if ((msg.command==1 && msg.data) || (msg.command==5) || (msg.command==3 && msg.clusterId==8)) {
-      lightLevel(msg.command, msg.data)
-    } else {
-      log.warn "Unknown command/data sequence received! Command: $msg.command Data: $msg.data"
-    }
-  } else if (description?.startsWith('temperature:')) {
-    // Log temperature received from switch
-    log.debug "Temperature is $description"
+  //log.debug msg
+  switch(msg.clusterId) {
+    case [6, 8]:
+      log.debug 'CONTROL'
+      resultMap = lightEvent(msg.command, msg.data)
+      break
+    case 32:
+      log.debug 'BATTERY'
+      resultMap = getBatteryResult(msg.value)
+      break
+    case 1026:
+      log.debug 'TEMPERATURE'
+      // temp is last 2 data values. reverse to swap endian
+      String temp = msg.data[-2..-1].reverse().collect { msg.hex1(it) }.join()
+      def value = getTemperature(temp)
+      resultMap = getTemperatureResult(value)
+      break
+    default:
+      log.debug "Unknown catchall message received!\n$msg"
   }
+  return resultMap
 }
 
 def configure() {
@@ -105,37 +137,95 @@ def configure() {
   return configCmds
 }
 
-def lightPower(command) {
-  if (command==0) {
-    log.debug "Turning light(s) off."
-    def result = createEvent(name: "button", value: "pushed", data: [buttonNumber: 1], descriptionText: "$device.displayName button 1 was pushed", isStateChange: true)
-    log.debug "Parse returned ${result?.descriptionText}"
-  } else {
-    log.debug "Turning light(s) on."
-    def result = createEvent(name: "button", value: "pushed", data: [buttonNumber: 2], descriptionText: "$device.displayName button 2 was pushed", isStateChange: true)
-    log.debug "Parse returned ${result?.descriptionText}"
-  }
-}
-
-def lightLevel(command, data) {
-  if (command==1) {
-    log.debug "Decreasing light(s) brightness."
-    def result = createEvent(name: "button", value: "held", data: [buttonNumber: 1], descriptionText: "$device.displayName button 1 was held", isStateChange: true)
-    log.debug "Parse returned ${result?.descriptionText}"
-    return result
-  } else if (command==5) {
-    log.debug "Increasing light(s) brightness."
-    def result = createEvent(name: "button", value: "held", data: [buttonNumber: 2], descriptionText: "$device.displayName button 2 was held", isStateChange: true)
-    log.debug "Parse returned ${result?.descriptionText}"
-    return result
-  } else {
-    log.debug "Stopping brightness change."
-    def result = createEvent(name: "button", value: "released", data: [buttonNumber: [1, 2]], descriptionText: "$device.displayName button 1 or 2 was released", isStateChange: true)
-    log.debug "Parse returned ${result?.descriptionText}"
-    return result
-  }
-}
-
 def refresh() {
   return zigbee.readAttribute(0x0402, 0x0000)
+}
+
+private lightEvent(command, data) {
+  Map resultMap = [:]
+  switch(command) {
+    case 0: //off command
+      log.debug 'Creating POWER OFF event'
+      resultMap = sendEvent(name: 'button 1', value: 'pressed', data: [buttonNumber: 1], descriptionText: "$device.displayName button 1 was pressed")
+    break
+    case 1: //on and brightness decrease command
+      if (data) {
+        log.debug 'Creating BRIGHTNESS DECREASE event'
+        resultMap = sendEvent(name: 'button 1', value: 'held', data: [buttonNumber: 1], descriptionText: "$device.displayName button 1 was held")
+      } else {
+        log.debug 'Creating POWER ON event'
+        resultMap = sendEvent(name: 'button 2', value: 'pressed', data: [buttonNumber: 2], descriptionText: "$device.displayName button 2 was pressed")
+      }
+    break
+    case 3: //brightness change stop command
+      log.debug 'Creating BRIGHTNESS STOP event'
+      resultMap = sendEvent(name: 'button', value: 'released', data: [buttonNumber: [1, 2]], descriptionText: "$device.displayName button 1 or 2 was released")
+    break
+    case 5: //brightness increase command
+      log.debug 'Creating BRIGHTNESS INCREASE event'
+      resultMap = sendEvent(name: 'button 2', value: 'held', data: [buttonNumber: 2], descriptionText: "$device.displayName button 2 was pressed")
+    break
+  }
+  return resultMap
+}
+
+private Map getBatteryResult(rawValue) {
+  def linkText = getLinkText(device)
+  def result = [
+    name: 'battery',
+    value: '--'
+  ]
+  log.debug rawValue
+  def volts = rawValue / 10
+  def descriptionText
+  if (rawValue == 0) {
+  } else {
+    if (volts > 3.5) {
+      result.descriptionText = "${linkText} battery has too much power (${volts} volts)."
+    } else if (volts > 0){
+      def minVolts = 2.1
+      def maxVolts = 3.0
+      def pct = (volts - minVolts) / (maxVolts - minVolts)
+      result.value = Math.min(100, (int) pct * 100)
+      result.descriptionText = "${linkText} battery was ${result.value}%"
+    }
+  }
+  return result
+}
+
+def getTemperature(value) {
+  def celsius = Integer.parseInt("$value", 16).shortValue() / 100
+  log.debug "Original value is $value and calculated celcius is $celsius"
+  if(getTemperatureScale() == "C"){
+    return celsius
+  } else {
+    def fahrenheit = celsiusToFahrenheit(celsius) as Integer
+    log.debug "Calculated fahrenheiht is $fahrenheit"
+    return fahrenheit
+    //return celsiusToFahrenheit(celsius) as Integer
+  }
+}
+
+private Map getTemperatureResult(value) {
+  def linkText = getLinkText(device)
+  if (tempOffset) {
+    def offset = tempOffset as int
+    def v = value as int
+    value = v + offset
+  }
+  def descriptionText = "${linkText} was ${value}°${temperatureScale}"
+  return [
+    name: 'temperature',
+    value: value,
+    descriptionText: descriptionText
+  ]
+}
+
+private Map parseCustomMessage(String description) {
+  Map resultMap = [:]
+  if (description?.startsWith('temperature: ')) {
+    def value = zigbee.parseHATemperatureValue(description, "temperature: ", getTemperatureScale())
+    resultMap = getTemperatureResult(value)
+  }
+  return resultMap
 }
