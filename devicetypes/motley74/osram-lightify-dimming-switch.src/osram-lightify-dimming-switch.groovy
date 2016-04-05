b1b136e853739e27dba23ba94ab6889aa50b5c2c
Michael Hudson <motley74@gmail.com>
2016-04-04 18:17:57 -0600

    Update OSRAM Lightify Dimming Switch and GoControl Wireless Remote Switch

--- devicetypes/motley74/osram-lightify-dimming-switch.src/osram-lightify-dimming-switch.groovy
+++ devicetypes/motley74/osram-lightify-dimming-switch.src/osram-lightify-dimming-switch.groovy
@@ -143,14 +143,14 @@ private Map parseCatchAllMessage(String description) {
           log.debug "Parse returned ${result?.descriptionText}"
           return result
           break
-        case 3: // brightness change stop command
-          //def result = [
-          //  name: 'button',
-          //  value: 'released',
-          //  data: [buttonNumber: [1,2]],
-          //  descriptionText: "$device.displayName button was released",
-          //  isStateChange: true
-          //]
+        case 3: /* brightness change stop command
+          def result = [
+            name: 'button',
+            value: 'released',
+            data: [buttonNumber: [1,2]],
+            descriptionText: "$device.displayName button was released",
+            isStateChange: true
+          ]*/
           log.debug "Recieved stop command, not currently implemented!"
           //return result
           break
